e7fa7d2954a2ace3e7e6324597ff84e0f55ee1a3
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-11 12:07:52 +0530

    first

---
+++ selenium/src/main/java/alert.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class alert {
+    public static void main (String[] arg) throws InterruptedException {
+
+        // To open the chrome using simple driver
+
+        //System.setProperty("webdriver.chrome.driver" , " location of the driver ")
+        ChromeDriver driver = new ChromeDriver();
+        //FirefoxDriver driver =
+
+            //load the url
+//        driver.get("https://letcode.in/");
+//        driver.get("https://letcode.in/login/");
+        driver.get("https://letcode.in/alert");
+
+        //accept the alert and get the text
+        driver.findElement(By.id("accept")).click();
+        Alert alert = driver.switchTo().alert();
+        String text = alert.getText();
+        System.out.println("simple text : " + text);
+        alert.accept();
+
+        //confirm alert
+        driver.findElement(By.id("confirm")).click();
+        Alert alertt = driver.switchTo().alert();
+        String textt = alert.getText();
+        System.out.println("simple text : " + textt);
+        alert.dismiss();
+
+        //prompt alert
+        driver.findElement(By.id("prompt")).click();
+        driver.switchTo().alert();
+        alert.sendKeys("selenium training shankar");
+        System.out.println(alert.getText());
+        alert.accept();
+        String name = driver.findElement(By.id("myName")).getText();
+        System.out.println(name);
+
+
+
+
+
+
+
+
+
+        //Thread.sleep(1000);
+        driver.quit();
+
+    }
+
+}
