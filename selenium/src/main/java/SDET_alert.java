adfa13f3daffa13372de3252424ee8c79069f3a1
jaishankar-2003 <jaishankar98421@gmail.com>
2025-08-20 09:18:49 +0530

    aug20

--- selenium/src/main/java/SDET_alert.java
+++ selenium/src/main/java/SDET_alert.java
@@ -3,15 +3,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;    // web driver is an interface
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.List;
 
 
 public class SDET_alert {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) //throws InterruptedException
+    {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://testautomationpractice.blogspot.com/");
+        //driver.get("https://testautomationpractice.blogspot.com/");
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
         driver.manage().window().maximize();
 
+        WebDriverWait mywait = new WebDriverWait(driver,Duration.ofSeconds(10)); //explicit wait declare
+
+        /*
         // normal alert ( driver.switchTo().alert().accept() )
         driver.findElement(By.xpath("//button[@id='alertBtn']")).click();
         Alert alert = driver.switchTo().alert();
@@ -31,7 +40,26 @@ public class SDET_alert {
 
 
 
+        driver.findElement(By.xpath("//button[@id='confirmBtn']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
 
+        driver.findElement(By.xpath("//button[@id='promptBtn']")).click();
+        Alert alert1 = driver.switchTo().alert();
+        alert1.sendKeys("onum ela");
+        System.out.println(alert1.getText());
+        alert1.accept();
+
+         */
+
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+
+        Alert alert = mywait.until(ExpectedConditions.alertIsPresent());
+
+
+        //Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
 
 
 
