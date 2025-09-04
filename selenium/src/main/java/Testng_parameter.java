274abc026acaa7735d60d2504767ba7db6c07351
jaishankar-2003 <jaishankar98421@gmail.com>
2025-09-04 21:51:20 +0530

    sep 4

---
+++ selenium/src/main/java/Testng_parameter.java
@@ -0,0 +1,45 @@
+import com.beust.jcommander.Parameter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class Testng_parameter
+{
+    WebDriver driver;
+    @BeforeTest
+    @Parameters({"browser"})
+    public void login(String br) throws InterruptedException
+    {
+        switch (br.toLowerCase())
+        {
+            case "chrome" : driver = new ChromeDriver(); break;
+            //case "edge" : driver = new EdgeDriver(); break;
+            case "firefox" : driver = new FirefoxDriver(); break;
+            default:System.out.println("invalid browser");
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+    }
+    @Test(priority = 1)
+    public void logo() throws InterruptedException
+    {
+        Thread.sleep(1000);
+        boolean status = driver.findElement(By.xpath("//img[@alt='company-branding']")).isDisplayed();
+        Assert.assertEquals(status,true);
+    }
+    @Test(priority = 2)
+    public void title()
+    {
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(),"OrangeHRM");
+    }
+}
